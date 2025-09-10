c399698e6189533b272030db14fa963651735fa1
Rosianu Calin <rosianu.calin@gmail.com>
2025-09-10 21:27:46 +0300

    made the borders rounded for blink

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -4,39 +4,39 @@ return {
   version = '1.*',
   dependencies = {
     'folke/lazydev.nvim',
-{
-        'L3MON4D3/LuaSnip',
-        version = 'v2.*',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-        opts = {},
+    {
+      'L3MON4D3/LuaSnip',
+      version = 'v2.*',
+      build = (function()
+        -- Build Step is needed for regex support in snippets.
+        -- This step is not supported in many windows environments.
+        -- Remove the below condition to re-enable on windows.
+        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+          return
+        end
+        return 'make install_jsregexp'
+      end)(),
+      dependencies = {
+        -- `friendly-snippets` contains a variety of premade snippets.
+        --    See the README about individual language/framework/plugin snippets:
+        --    https://github.com/rafamadriz/friendly-snippets
+        -- {
+        --   'rafamadriz/friendly-snippets',
+        --   config = function()
+        --     require('luasnip.loaders.from_vscode').lazy_load()
+        --   end,
+        -- },
       },
+      opts = {},
+    },
   },
   opts = {
     keymap = {
       preset = 'default',
       ['<C-e>'] = show,
       ['<C-space>'] = {
-        function (cmp)
-          cmp.show({ providers = {'snippets'},})
+        function(cmp)
+          cmp.show({ providers = { 'snippets' }, })
         end
       },
     },
@@ -55,24 +55,25 @@ return {
         show_on_accept_on_trigger_character = false,
         show_on_blocked_trigger_characters = { "{", "(", ")", "}" },
       },
-      documentation = {auto_show = false, auto_show_delay_ms = 500},
+      documentation = { auto_show = false, auto_show_delay_ms = 500 },
       ghost_text = {
         enabled = true,
       },
       menu = {
+        border = 'rounded',
         auto_show = true,
       },
     },
     sources = {
-      default = {'lsp', 'path', 'snippets', 'lazydev'},
+      default = { 'lsp', 'path', 'snippets', 'lazydev' },
       providers = {
-        lazydev = {module = 'lazydev.integrations.blink', score_offset = 100},
+        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
       },
     },
 
     snippets = { preset = 'luasnip' },
-    fuzzy = {preset='lua'},
+    fuzzy = { preset = 'lua' },
 
-    signature = {enabled = true},
+    signature = { enabled = true },
   },
 }
